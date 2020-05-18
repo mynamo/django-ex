e7d5abeb096c1d3ad096ace4e4f91cb33e32ffea
Aditi <adkulkar@redhat.com>
2020-05-17 20:26:33 -0700

    Hello world with django

--- project/urls.py
+++ project/urls.py
@@ -1,21 +1,6 @@
-from django.conf import settings
-from django.conf.urls import include, url
-from django.contrib import admin
-
-from welcome.views import index, health
+from django.conf.urls import url
+from django.urls import include
 
 urlpatterns = [
-    # Examples:
-    # url(r'^$', 'project.views.home', name='home'),
-    # url(r'^blog/', include('blog.urls')),
-
-    url(r'^$', index),
-    url(r'^health$', health),
-    url(r'^admin/', include(admin.site.urls)),
+    url(r'^APP/', include('APP.urls')),
 ]
-
-if settings.DEBUG:
-    import debug_toolbar
-    urlpatterns = [
-        url(r'^__debug__/', include(debug_toolbar.urls)),
-    ] + urlpatterns
